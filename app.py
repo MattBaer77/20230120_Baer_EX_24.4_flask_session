6cee6f89c2d3d5ff7657aa5504570d588134b758
Matthew Baer <matt.baer77@gmail.com>
2023-01-22 16:55:37 -0500

    session added - back button error fixed

--- app.py
+++ app.py
@@ -1,66 +1,97 @@
 # First we must import Flask
 # and by importing a flask object called request 
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 
-#importing surveys.py
-import surveys
+# Importing surveys.py
+from surveys import surveys
 
-#  Importing the debug toolbar
+# Importing the debug toolbar
 from flask_debugtoolbar import DebugToolbarExtension
 
 # This command initiates server - tells flask to do it's thing in the file.
 app = Flask(__name__)
 
-# Secret key for some reason
 app.config['SECRET_KEY'] = "george_costanza"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-
 debug = DebugToolbarExtension(app)
 
-responses = []
+
+
+
+current_survey = 'satisfaction'
 
 @app.route('/')
 def home_page():
-    survey_title = surveys.satisfaction_survey.title
-    return render_template('root.html', survey_title=survey_title, index=len(responses))
+    # survey_title = surveys.satisfaction_survey.title
+    survey_title = surveys[current_survey].title
+
+    # session['responses'] = []
+
+    return render_template(
+        'root.html',
+        survey_title=survey_title,
+        )
+
+
+@app.route('/startsurvey', methods=['POST'])
+def start_survey():
+    session["responses"] = []
+
+    return redirect('/questions/0')
+
 
 @app.route('/questions/<int:questidx>')
 def question(questidx):
 
-    if (len(responses) != questidx):
-        flash('Please answer the current question.')
-        return redirect(f'/questions/{len(responses)}')
+    if (len(session["responses"]) != questidx):
+        flash('A - Please answer the current question.')
+        return redirect(f'/questions/{len(session["responses"])}')
 
-    survey_question = surveys.satisfaction_survey.questions[questidx].question
-    survey_choices = surveys.satisfaction_survey.questions[questidx].choices
+    survey_question = surveys[current_survey].questions[questidx].question
+    survey_choices = surveys[current_survey].questions[questidx].choices
 
-    return render_template('question_page.html',
-    this_question=questidx,
-    survey_choices=survey_choices,
-    question=survey_question)
+    return render_template(
+        'question_page.html',
+        this_question=questidx,
+        survey_choices=survey_choices,
+        question=survey_question
+        )
 
-@app.route('/answer', methods=['POST'])
-def answer():
+
+@app.route('/answer/<int:answeridx>', methods=['POST'])
+def answer(answeridx):
 
     if request.form == {}:
-        flash('Please answer the current question.')
-        return redirect(f'/questions/{len(responses)}')
+        flash('B - Please answer the current question.')
+        return redirect(f'/questions/{len(session["responses"])}')
 
     answer = request.form['options']
-    responses.append(answer)
 
-    if len(responses) == len(surveys.satisfaction_survey.questions):
+    response_list = session["responses"]
+
+
+    if len(session["responses"]) == answeridx:
+        response_list.append(answer)
+        session["responses"] = response_list
+    else:
+        response_list[answeridx] = answer
+        session["responses"] = response_list
+
+
+    if len(session["responses"]) == len(surveys[current_survey].questions):
         return redirect('/thankyou')
 
-    return redirect(f'/questions/{len(responses)}')
+    return redirect(f'/questions/{len(session["responses"])}')
+
 
 @app.route('/thankyou')
 def thankyou():
-    print(responses)
 
-    if len(responses) < len(surveys.satisfaction_survey.questions):
-        flash('Please answer the current question')
-        return redirect(f'/questions/{len(responses)}')
+    print(session["responses"])
+
+    if len(session["responses"]) < len(surveys[current_survey].questions):
+        flash('C - Please answer the current question')
+        return redirect(f'/questions/{len(session["responses"])}')
 
     return render_template('thankyou.html')
